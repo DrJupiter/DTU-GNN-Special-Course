torch implementation to fix our numerical stability

torch_model/PaiNN.py
@@ -2,11 +2,12 @@ import torch
 from torch import nn
 from torch_model.message import Message
 from torch_model.update_fn import Update
-from torch_scatter import scatter_add
+from torch_scatter import scatter
 
 class PaiNN(nn.Module):
 
     def __init__(self, vocab_dim, feature_dim):
+        super().__init__()
         self.embeddings = nn.Embedding(vocab_dim, feature_dim)
 
         self.lin_1 = nn.Linear(feature_dim, feature_dim)
@@ -58,10 +59,9 @@ class PaiNN(nn.Module):
         s = self.lin_2(s)
         # M x R x F
         
-        return scatter_add(s, idx_m).sum(dim=-1).squeeze()
+        return scatter(s, idx_m, dim=0).sum(dim=-1).squeeze()
 
 if __name__ == "__main__":
-    import jax
     from experiment.config import BuilderConfigExperiment
     from transformers import set_seed
     from data.dataloader import construct_dataloaders
@@ -70,76 +70,36 @@ if __name__ == "__main__":
     config = (
         BuilderConfigExperiment()
         .set_vocab_dim(20)
-        .set_feature_dim(256)
-        .set_train_size(150_000)
+        .set_feature_dim(128)
+        .set_train_size(100_000)
         .set_validation_size(15_000)
         .set_batch_size(2)
         .set_num_workers(6)
         .set_radius(3.)
         .set_path("./data/qm9.db")
-        .set_split_file("./data/split.npz")
+        .set_split_file("./data/split2.npz")
         .build()
     )
 
     train, test, val = construct_dataloaders(config.train)
 
     data_point = (next(iter(train)))
-    positions = jax.numpy.array(data_point["_positions"].numpy())
-    atomic_numbers = jax.numpy.array(data_point["_atomic_numbers"].numpy())
-    idx_i, idx_j = jax.numpy.array(data_point["_idx_i"].numpy()), jax.numpy.array(data_point["_idx_j"].numpy())
-    idx_m = jax.numpy.array(data_point["_idx_m"].numpy())
-    s0 = atomic_numbers[:, jax.numpy.newaxis]
-    v0 = jax.numpy.zeros((len(atomic_numbers), 3, config.model.feature_dim))
+    positions = data_point["_positions"]
+    atomic_numbers = data_point["_atomic_numbers"]
+    idx_i, idx_j = data_point["_idx_i"], data_point["_idx_j"]
+    idx_m = data_point["_idx_m"]
+    s0 = atomic_numbers[:, None]
+    v0 = torch.zeros((len(atomic_numbers), 3, config.model.feature_dim))
+    
 
-
-    ### init ###
-    #N, feature_dim, vocab_dim = 3, 128, 10
-
-    model, params = construct_PaiNN(vocab_dim=config.model.vocab_dim, feature_dim=config.model.feature_dim)
-
-    key = jax.random.PRNGKey(42)
-    split_keys = jax.random.split(key,4)
-
-    #v0 = jax.random.normal(split_keys[0], (N, 3, feature_dim))
-    #s0 = jax.random.normal(split_keys[1], (N, 1, vocab_dim))
-    #positions = jax.random.normal(split_keys[2], (N, 3))
-
-    #idx_i = jax.numpy.array([0, 0, 1, 2])
-    #idx_j = jax.numpy.array([1,2,0,0])
-    #idx_m = jax.numpy.array([0,0,1])
+    model = PaiNN(config.model.vocab_dim, config.model.feature_dim)
 
     dir_i_to_j = lambda x, i, j: x[j] - x[i]
     directions = dir_i_to_j(positions, idx_i, idx_j)
 
     ### Basic test ###
 
-    predictions = model(params, v0, s0, directions, idx_i=idx_i, idx_j = idx_j, idx_m=idx_m)
+    predictions = model(v0, s0, directions, idx_i=idx_i, idx_j = idx_j, idx_m=idx_m)
 
     assert predictions.shape == (max(idx_m)+1,), (predictions, predictions.shape, max(idx_m) + 1) # s0.shape[:-1]
-
-    ### Jit test ###
-
-    #model = jax.jit(model)
-    #predictions = model(params, v0, s0, r)
-    #assert predictions.shape == (N,1)
-
-    ### Grad test ###
-
-    target = jax.random.normal(jax.random.PRNGKey(0), (max(idx_m) + 1,))
-    target = jax.numpy.array(data_point['energy_U0'].numpy())
-    data = [v0,s0,directions, idx_i, idx_j, idx_m]
-
-    loss_fn = lambda params, data, target: (
-        (model(params, *data) - target) ** 2
-    ).mean()
-    grad_fn = jax.grad(loss_fn)
-
-    grads = grad_fn(params, data, target)
-    LEARNING_RATE = 1e-2
-
-    p_update_fn = jax.tree.map(
-        lambda p, g: p - LEARNING_RATE * g, params, grads
-    )
-
-    # Check that all layers's weights are being updated
-    assert jnp.abs(1-jnp.isclose(jnp.array([jnp.abs(leaf).sum() for leaf in jax.tree.leaves(grads)]), 0)).prod()
+    print(predictions)

torch_model/message.py
@@ -14,7 +14,7 @@ def temporary_sys_path(path):
 
 import torch
 from torch import nn
-from torch_scatter import scatter_add
+from torch_scatter import scatter_add, scatter
 
 
 def min_max(a,b):
@@ -95,14 +95,13 @@ class Message(nn.Module):
         # EDGES x 3 x F -> A x 3 x F
 
         # aggregate
-
         # EDGES x 1 x F -> A x 1 x F
-        delta_s = scatter_add(delta_s, idx_j)
+        delta_s = scatter(delta_s, idx_j, dim=0)
         # EDGES x 3 x F -> A x 3 x F
-        delta_v = scatter_add(delta_v, idx_j)
+        delta_v = scatter(delta_v, idx_j, dim=0)
 
-        assert delta_v.shape == vectors.shape
-        assert delta_s.shape == delta_s.shape
+        assert delta_v.shape == vectors.shape, f"{delta_v.shape} != {vectors.shape}"
+        assert delta_s.shape == scalars.shape, f"{delta_s.shape} != {scalars.shape}"
 
         return delta_v, delta_s
 

torch_model/update_fn.py
@@ -16,7 +16,8 @@ class Update(nn.Module):
     def forward(self, vectors, scalars):
         u = self.lin_u(vectors)
         v = self.lin_v(vectors)
-
+        
+        # A x 3 x F
         v_norm = torch.linalg.norm(v, dim=-2, keepdim=True)
 
         s = torch.concat((scalars, v_norm), dim=-1)